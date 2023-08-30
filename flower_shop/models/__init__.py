166e0f29ccd5fd2455071660a210403fdd679b0b
Mehdi Bendali Hacine <mebe@odoo.com>
2023-08-30 07:47:45 +0400

    Remove module prior to review

--- flower_shop/models/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from . import flower_flower
-from . import product_template
-from . import flower_water
-from . import stock_lot
-from . import stock_warehouse_weather
--- flower_shop/models/flower_water.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class FlowerWater(models.Model):
-    _name = 'flower.water'
-    _description = 'Flower Watering'
-
-    date = fields.Date(string="Watering Date", default=fields.Date.context_today)
-    lot_id = fields.Many2one('stock.lot', string="Plant")
